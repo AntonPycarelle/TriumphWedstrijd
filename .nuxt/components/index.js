eecc90a368aa359d26600341526205705ed6d8cc
Anton Pycarelle <antonpycarelle@MacBook-Pro-van-Anton.local>
2020-12-10 10:32:59 +0100

    Working Form

---
+++ .nuxt/components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer } from '../../components/Footer.vue'
+export { default as Form } from '../../components/Form.vue'
+export { default as Logo } from '../../components/Logo.vue'
+
+export const LazyFooter = import('../../components/Footer.vue' /* webpackChunkName: "components/Footer" */).then(c => c.default || c)
+export const LazyForm = import('../../components/Form.vue' /* webpackChunkName: "components/Form" */).then(c => c.default || c)
+export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo" */).then(c => c.default || c)
