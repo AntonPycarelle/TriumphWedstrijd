62859a1a5e9bf408bb67751ca24475fe209f51e7
Anton Pycarelle <antonpycarelle@MacBook-Pro-van-Anton.local>
2020-12-11 11:58:53 +0100

    Title aanpassing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Triumph',
+    title: 'De Gemaskerde Motard | Triumph',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
